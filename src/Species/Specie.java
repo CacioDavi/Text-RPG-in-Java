da198a827b08d51424ac4012a0c6cbb29e19cfd7
CacioDavi <davicacio4@gmail.com>
2026-06-21 22:52:47 -0300

    Initial Classes increment

--- src/Species/Specie.java
+++ src/Species/Specie.java
@@ -1,9 +1,19 @@
 package Species;
 
-public abstract class Specie {
-    public String Name;
+public enum Specie {
+    Human("Human", 23f, 10f, 50f),
+    Elf("Elf", 20f, 30f, 30f),
+    Dwarf("Dwarf", 25f, 15f, 30f);
+
+    private String Name;
+    private Float baseLife;
+    private Float baseVelocity;
+    private Float baseStrength;
     
-    public static void main() {
-        
+    private Specie(String Name, Float baseLife, Float baseVelocity, Float baseStrength) {
+        this.Name = Name;
+        this.baseLife = baseLife;
+        this.baseVelocity = baseVelocity;
+        this.baseStrength = baseStrength;
     }
 }
